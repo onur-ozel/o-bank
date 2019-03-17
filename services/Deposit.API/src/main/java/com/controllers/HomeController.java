c06d42b7ade14621026851f010cf945cae261587
onurozelgit <onurozel41@gmail.com>
2019-03-18 00:28:21 +0300

    swagger added

---
+++ services/Deposit.API/src/main/java/com/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // note - this is a spring-boot controller, not @RestController
+public class HomeController {
+    @RequestMapping ("/swagger")
+    public String home() {
+	return "redirect:/swagger-ui.html";
+    }
+}
--- services/Deposit.API/src/main/java/com/controllers/TimeDepositContoller.java
+++ services/Deposit.API/src/main/java/com/controllers/TimeDepositContoller.java
@@ -3,16 +3,24 @@ package com.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 /**
  * TimeDeposit
  */
 @RestController
-@RequestMapping("/TimeDeposit")
+@RequestMapping("/timedeposit")
+@Api(value = "HelloWorld Resource", description = "shows hello world")
 public class TimeDepositContoller {
 
     @RequestMapping("/items")
+    @ApiOperation(value = "Returns Hello World")
+    @ApiResponses(value = { @ApiResponse(code = 100, message = "100 is the message"),
+            @ApiResponse(code = 200, message = "Successful Hello World") })
     public String name() {
         return "AAAA";
     }
 }
-
