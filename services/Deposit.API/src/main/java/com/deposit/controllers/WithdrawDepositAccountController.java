ec25f9148af7284771d7e7947b2ceb909300ae47
onur <45355705+onur-ozel@users.noreply.github.com>
2019-03-28 11:45:11 +0300

    Update WithdrawDepositAccountController.java

--- services/Deposit.API/src/main/java/com/deposit/controllers/WithdrawDepositAccountController.java
+++ services/Deposit.API/src/main/java/com/deposit/controllers/WithdrawDepositAccountController.java
@@ -11,30 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// import io.swagger.annotations.Api;
-// import io.swagger.annotations.ApiOperation;
-// import io.swagger.annotations.ApiParam;
-// import io.swagger.annotations.ApiResponse;
-// import io.swagger.annotations.ApiResponses;
-
 @RestController
 @RequestMapping("deposit/api/v1/withdraw-deposit-accounts")
-// @Api(value = "Withdraw Deposit Account Controller", description = "Withdraw
-// Deposit Account Operations.")
 public class WithdrawDepositAccountController {
 
     @Autowired
     WithdrawDepositAccountService service;
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
-    // @ApiOperation(value = "View a list of available withdraw deposit accounts.
-    // For paging send 'offset' and 'limit' othervise returns full data. For sorting
-    // send 'sort' ")
-    // @ApiResponses(value = {
-    // @ApiResponse(code = 200, message = "Successfully retrieved list",
-    // responseContainer = "List", response = WithdrawDepositAccount.class),
-    // @ApiResponse(code = 404, message = "The resource you were trying to reach is
-    // not found") })
     public String get(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit,
             @RequestParam("sorts") String sorts, @RequestParam("fields") String fields,
             @RequestParam("searches") String searches) throws JsonProcessingException {
@@ -42,7 +26,6 @@ public class WithdrawDepositAccountController {
         return service.get(offset, limit, sorts, fields, searches);
     }
 
-    // @ApiOperation(value = "Add new withdraw deposit account")
     @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
     public void add(@RequestBody WithdrawDepositAccount account) {
         service.add(account);
