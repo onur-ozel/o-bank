75bf598bf4dcb97149df6e21f967c3028a3c52cd
onur <45355705+onur-ozel@users.noreply.github.com>
2019-03-30 18:39:02 +0300

    Update WithdrawDepositAccountController.java

--- services/Deposit.API/src/main/java/com/deposit/controllers/WithdrawDepositAccountController.java
+++ services/Deposit.API/src/main/java/com/deposit/controllers/WithdrawDepositAccountController.java
@@ -1,10 +1,13 @@
 package com.deposit.controllers;
 
+import java.util.Optional;
+
 import com.deposit.models.WithdrawDepositAccount;
 import com.deposit.services.WithdrawDepositAccountService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,9 +22,11 @@ public class WithdrawDepositAccountController {
     WithdrawDepositAccountService service;
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
-    public String get(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit,
-            @RequestParam("sorts") String sorts, @RequestParam("fields") String fields,
-            @RequestParam("searches") String searches) throws JsonProcessingException {
+    public String get(@RequestParam(name = "offset", required = false) Integer offset,
+            @RequestParam(name = "limit", required = false) Integer limit,
+            @RequestParam(name = "sorts", required = false) String sorts,
+            @RequestParam(name = "fields", required = false) String fields,
+            @RequestParam(name = "searches", required = false) String searches) throws JsonProcessingException {
 
         return service.get(offset, limit, sorts, fields, searches);
     }
@@ -29,5 +34,20 @@ public class WithdrawDepositAccountController {
     @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
     public void add(@RequestBody WithdrawDepositAccount account) {
         service.add(account);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT, produces = "application/json")
+    public void update(@RequestBody WithdrawDepositAccount account) {
+        service.update(account);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public void delete(@PathVariable(name = "id", required = true) String id) {
+        service.delete(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Optional<WithdrawDepositAccount> getById(@PathVariable(name = "id", required = true) String id) {
+        return service.getById(id);
     }
 }
